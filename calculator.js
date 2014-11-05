1a3fef4bae76503873b1a461656f60db4af2b119
maniart <mani.art@gmail.com>
2014-11-04 23:54:24 -0500

    sum all

--- calculator.js
+++ calculator.js
@@ -57,7 +57,7 @@ var defineBuckets = function(categories) {
 		_.extend(
 			category, { bucket : _.find(
 				buckets, function(bucket) {
-					console.log(category.sum, ' ', bucket.min, ' ', bucket.max, '\n');
+					//console.log(category.sum, ' ', bucket.min, ' ', bucket.max, '\n');
 
 					return category.factor >= bucket.min && category.factor <= bucket.max; 
 				}
@@ -80,13 +80,22 @@ var addBucketWeight = function(categories) {
 
 var defineBucketCode = function(categories) {
 
+	var code;
+	console.log('________________________________________________________________');
 	_.each(categories, function(category){
+		/*
+		if(category.bucket.zone === 1) {
+			code = [1, 0, 0];
+		} else if(category.bucket.zone === 2) {
+			code = [0, 1, 0];
+		} else {
+			code = [0, 0, 1];
+		}
+		*/
+		console.log(category.bucket);
 		_.extend(
 			category, { 
-				code : category.bucket.zone === 1 ? [1, 0, 0] : 
-					category.bucket.zone === 2 ? 
-					[0, 1, 0] : 
-					[0, 0, 1]  
+				code : code 
 			}
 		);
 	});
@@ -94,23 +103,64 @@ var defineBucketCode = function(categories) {
 	return categories;
 };
 
+sumAllCategories = function(categories) {
+
+	return _.reduce(
+		categories, function(memo, category, index, list) {
+			return category.sum + memo;
+		}, 0
+	);
+
+	return sum;
+
+};
+
+var addZoneWeight = function(categories) {
+
+	return _.reduce(
+		categories, function(memo, category, index, list){
+			_.each(category.code, function(digit, index, list){
+				memo[index] += digit;
+			});
+		}, [0, 0, 0]
+	);
+
+};
+
 var calculate = function(collection) {
-	
-	console.log(util.inspect(
-		addBucketWeight(
-			defineBucketCode(
-				defineBuckets(
-					calculateCategorySum(
-						calculateCategoryFactors(
-							merge(collection)
+				
+				console.log(
+					sumAllCategories(
+						defineBucketCode(
+							defineBuckets(
+								calculateCategorySum(
+									calculateCategoryFactors(
+										merge(collection)
+									)
+								)
+							)
 						)
 					)
-				)
-			)
-		), 
+				);
 
+
+	/*
+	console.log(util.inspect(
+					defineBucketCode(
+						defineBuckets(
+							calculateCategorySum(
+								calculateCategoryFactors(
+									merge(collection)
+								)
+							)
+						)
+					)
+				
+			
+		,
 		{showHidden: false, depth: null})
 	);
+*/
 	
 };
 
