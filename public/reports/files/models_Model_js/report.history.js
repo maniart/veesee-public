44e0fe85f978a5c63e8caac33431fac21acf9d2a
maniart <mani.art@gmail.com>
2015-01-13 10:27:04 -0500

    Implemented Browserify workflow
    * Added Browserify
    * Added Bower for front-end deps
    * Added Grunt tasks for Bower and Browserify
    * Added browserify-shim

--- public/reports/files/models_Model_js/report.history.js
+++
@@ -1,1 +0,0 @@
-__history = [{"date":"Tue, 18 Feb 2014 05:02:09 GMT","sloc":30,"lloc":6,"functions":3,"deliveredBugs":0.05270780907334513,"maintainability":84.10174117877301,"lintErrors":0,"difficulty":7.5},{"date":"Tue, 21 Oct 2014 22:58:38 GMT","sloc":30,"lloc":6,"functions":3,"deliveredBugs":0.05270780907334513,"maintainability":84.10174117877301,"lintErrors":0,"difficulty":7.5}]
