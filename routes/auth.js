ffff1d1607a1d11d6f679d4d6db106aaa1ab55f7
maniart <mani.art@gmail.com>
2015-02-08 16:01:18 -0500

    WIP auth - working on `_.omit` from `user` on `signup` callback

--- routes/auth.js
+++ routes/auth.js
@@ -68,11 +68,11 @@ router.post('/signup', function(req, res) {
         } else {
            // Set the user cookies and return the cleansed user data
             //console.log(user);
-            console.log('>>>>>>>>>>> user is: ', user.auth_token);
+            console.log('>>>>>>>>>>> user omit: ',  _.omit(user, ['password', 'auth_token']));
             
             res.cookie('user_id', user._id, { signed: true, maxAge: config.cookieMaxAge  });
             res.cookie('auth_token', user.auth_token, { signed: true, maxAge: config.cookieMaxAge  });
-            res.json({ user: _.omit(user, ['password', 'authToken']) });    
+            res.json({ user: _.omit(user, ['password', 'auth_token']) });    
         }
     });
 });
@@ -100,97 +100,4 @@ router.post('/remove_account', function(req, res) {
     });
 });
 
-/*
-app.get("/api/auth", function(req, res){
-    db.get("SELECT * FROM users WHERE id = ? AND auth_token = ?", [ req.signedCookies.user_id, req.signedCookies.auth_token ], function(err, user){
-        if(user){
-            res.json({ user: _.omit(user, ['password', 'auth_token']) });   
-        } else {  
-            res.json({ error: "Client has no valid login cookies."  });   
-        }
-    });
-});
-*/
-
-// POST /api/auth/login
-// @desc: logs in a user
-/*
-app.post("/api/auth/login", function(req, res){
-    db.get("SELECT * FROM users WHERE username = ?", [ req.body.username ], function(err, user){
-        if(user){
-
-            // Compare the POSTed password with the encrypted db password
-            if( bcrypt.compareSync( req.body.password, user.password)){
-                res.cookie('user_id', user.id, { signed: true, maxAge: config.cookieMaxAge  });
-                res.cookie('auth_token', user.auth_token, { signed: true, maxAge: config.cookieMaxAge  });
-
-                // Correct credentials, return the user object
-                res.json({ user: _.omit(user, ['password', 'auth_token']) });   
-
-            } else {
-                // Username did not match password given
-                res.json({ error: "Invalid username or password."  });   
-            }
-        } else {
-            // Could not find the username
-            res.json({ error: "Username does not exist."  });   
-        }
-    });
-});
-*/
-// POST /api/auth/signup
-// @desc: creates a user
-/*
-app.post("/api/auth/signup", function(req, res){
-    db.serialize(function(){
-        db.run("INSERT INTO users(username, name, password, auth_token) VALUES (?, ?, ?, ?)", 
-                [ req.body.username, req.body.name, bcrypt.hashSync(req.body.password, 8), bcrypt.genSaltSync(8) ], function(err, rows){
-            if(err){
-                res.json({ error: "Username has been taken.", field: "username" }); 
-            } else {
-
-                // Retrieve the inserted user data
-                db.get("SELECT * FROM users WHERE username = ?", [ req.body.username ], function(err, user){
-                    if(!user) {
-                        console.log(err, rows);
-                        res.json({ error: "Error while trying to register user." }); 
-                    } else {
-
-                        // Set the user cookies and return the cleansed user data
-                        res.cookie('user_id', user.id, { signed: true, maxAge: config.cookieMaxAge  });
-                        res.cookie('auth_token', user.auth_token, { signed: true, maxAge: config.cookieMaxAge  });
-                        res.json({ user: _.omit(user, ['password', 'auth_token']) });   
-                    }
-                });
-            }
-        });
-    });
-});
-*/
-
-// POST /api/auth/logout
-// @desc: logs out a user, clearing the signed cookies
-/*
-app.post("/api/auth/logout", function(req, res){
-    res.clearCookie('user_id');
-    res.clearCookie('auth_token');
-    res.json({ success: "User successfully logged out." });
-});
-*/
-
-// POST /api/auth/remove_account
-// @desc: deletes a user
-/*
-app.post("/api/auth/remove_account", function(req, res){
-    db.run("DELETE FROM users WHERE id = ? AND auth_token = ?", [ req.signedCookies.user_id, req.signedCookies.auth_token ], function(err, rows){
-        if(err){ 
-            res.json({ error: "Error while trying to delete user." }); 
-        } else {
-            res.clearCookie('user_id');
-            res.clearCookie('auth_token');
-            res.json({ success: "User successfully deleted." });
-        }
-    });
-});
-*/
 module.exports = router;
