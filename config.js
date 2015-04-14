10f8e28dbeb9967ccf4a6f2e923f56699eaa43a0
Mani Nilchiani <mani.art@gmail.com>
2015-04-14 18:11:08 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,1 @@
-/*
-	@module config
-*/
-var config = {
-	MONGODB_URL: 'mongodb://localhost/veesee',
-    sessionSecret: 'bb-login-secret',
-    cookieSecret: 'bb-login-secret',
-    cookieMaxAge: (1000 * 60 * 60 * 24 * 365)
-};
- 
-module.exports = config;
+/* private */
